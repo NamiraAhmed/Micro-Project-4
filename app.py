a92647357f70a264c237d8f3328e12af34f2b0fb
NamiraAhmed <62087080+NamiraAhmed@users.noreply.github.com>
2021-05-22 21:02:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import requests
+from flask import Flask, render_template, request
+from twilio.rest import Client
+import requests
+import requests_cache
+account_sid='AC71e591015d81df3031778dbf7399db45'
+auth_token='efc4820d57a3b0f38fec585ef3579534'
+client = Client(account_sid, auth_token)
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/')
+def registration_form():
+    return render_template('Login_Page.html')
+
+
+@app.route('/status', methods=['POST'])
+def login_registration_details():
+    first_name=request.form['fname']
+    last_name=request.form['lname']
+    email_id=request.form['email']
+    source_st=request.form['source_state']
+    source_dt=request.form['source']
+    destination_st=request.form['dest_state']
+    destination_dt=request.form['destination']
+    phoneNumber=request.form['phoneNumber']
+    id_proof=request.form['aadhaarnumber']
+    date =request.form['date']
+    full_name= first_name+ "." + last_name
+    r= requests.get('https://api.covid19india.org/v4/data.json')
+    json_data=r.json()
+    cnt=json_data[destination_st]['districts'][destination_dt]['total']['confirmed']
+    pop=json_data[destination_st]['districts'][destination_dt]['meta']['population']
+    travel_pass=((cnt/pop)*100)
+    if travel_pass <30:
+        status = 'CONFIRMED'
+        client.messages.create(to="whatsapp:+91"+phoneNumber,
+                              from_="whatsapp:+14155238886",
+                              body="Hello "+full_name+"! Your Travel From "+source_dt+" To "+destination_dt+" Has "+status+" On "+date)
+        return render_template('User_Registration_Details.html',var=full_name, var1=email_id,var2=id_proof,var3=source_st,var4=source_dt,var5=destination_st,var6=destination_dt,var7=phoneNumber,var8=date,var9=status)
+    else:
+        status='Not Confirmed'
+        client.messages.create(to="whatsapp:+91"+phoneNumber,
+                              from_="whatsapp:+14155238886",
+                              body="Hello " + full_name + "! Your Travel From " + source_dt + " To " + destination_dt + " Has " + status + " On " + date + " Apply Later")
+        return render_template('User_Registration_Details.html', var=full_name, var1=email_id, var2=id_proof,
+                               var3=source_st, var4=source_dt, var5=destination_st, var6=destination_dt,
+                               var7=phoneNumber, var8=date, var9=status)
+
+        if __name__ == "__main__":
+            app.run(port=9001, debug=True)
